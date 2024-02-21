38c1ef4cf1971264b7ad3132990d46bcac6342bf
Andrea Fabbricatore <andy.fabb10@gmail.com>
2024-02-21 21:43:00 +0100

    grammar fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <main className="main">
       <Hero></Hero>
       <Content
-        title="What exactly do we do?"
-        desc1="We leverage our network of experts and professionals in the field of AI to develop custom solutions for businesses that are wanting to automate or innovate their processes and products. When you come to us, we will analyze your case in depth and personally take care of the whole development process - from allocating the right people, to polishing the end result and implementing it in your current systems."
+        title="What we do"
+        desc1="We leverage our network of experts and professionals in the field of AI to develop custom solutions for businesses that are wanting to automate or innovate their processes and products. When you come to us, we will analyze your case in depth and personally take care of the whole development process - from allocating the right people, to polishing the end result and implementing it into your current systems."
         desc2="If you want to bring AI into your business but don't know how, then we will be your state-of-the-art AI team for the duration of the project and, if needed, even further."
         img1="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
         img2="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
